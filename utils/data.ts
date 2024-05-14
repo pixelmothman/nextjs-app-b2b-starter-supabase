aa26d4e1fba8361588660ce75b6eb5d51b1768ee
PixelMothman <63313404+pixelmothman@users.noreply.github.com>
2024-05-14 09:38:35 -0300

    fix: fix data.ts to fetch org membership data instead of trip membership data

--- utils/data.ts
+++ utils/data.ts
@@ -26,19 +26,19 @@ export async function getOrgs(){
         //delete previous instance of supabase client and start using supabase client with server role now
         supabase = createSupaServerClient();
 
-        //fetch the trip ids of the trips that the user is part of
-        const {data: orgMembershipData, error: orgMembershipDataError} = await supabase.from("trip_membership_table").select('trip_id').eq('user_id', user.id).neq('status', 'pending');
-        if (tripMembershipDataError) {
-            throw new DBError('Failed to fetch trip membership data');
-        }
+        //fetch the orgs that the user is part of
+        const {data: orgMembershipData, error: orgMembershipDataError} = await supabase.from("org_membership_table").select().eq('user_id', user.id)
+        if (orgMembershipDataError) {
+            throw new DBError('Failed to fetch org membership data');
+        };
         
-        //if the user is not a member of any trips, return
-        if(!tripMembershipData || tripMembershipData.length === 0){
+        //if the user is not a member of any orgs, return
+        if(!orgMembershipData || orgMembershipData.length === 0){
             return [];
         };
         
-        return tripsData;
-    } catch (e) {
+        return orgMembershipData;
+    } catch (e: any) {
         return handleError(e);
     };
 };
