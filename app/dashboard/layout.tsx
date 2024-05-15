3b5eed539d5968fa1cc47388d7aacbc025ef4519
PixelMothman <63313404+pixelmothman@users.noreply.github.com>
2024-05-15 08:45:50 -0300

    refactor: Remove DashboardHeader component, update DashboardLayout to remove dependency on it, and delete code from DashboardLayout layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,8 +3,6 @@
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 import { type ReactNode } from 'react';
-import DashboardHeader from '@/components/dashboard/dashboardHeader';
-import { getOrgs } from '@/utils/data';
 
 export default async function DashboardLayout( {children}: {children: ReactNode} ) {
     const supabase = createClient();
@@ -15,22 +13,9 @@ export default async function DashboardLayout( {children}: {children: ReactNode}
         redirect('/');
     };
 
-    //if the user does not belong to an organization, redirect to the organization creation page
-    const isInOrg = await getOrgs();
-    // Check if the response is an error
-    if ('error' in isInOrg!) {
-        redirect(`/org/error?error_message=${isInOrg.error.message}&error_code=${isInOrg.error.code}&error_details=${isInOrg.error.details}`)      
-    };
-    if (!isInOrg || isInOrg.length === 0) {
-        redirect('/org/create');
-    };
-
     return (
         <div className="w-full h-full flex-col bg-neutral-100 overflow-hidden">
-            <DashboardHeader/>
-            <div className="w-full h-full flex flex-row">
             {children}
-            </div>
         </div>
     )
 };
