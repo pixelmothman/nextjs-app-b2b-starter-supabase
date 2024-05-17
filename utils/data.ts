05c9930212404f834019477340b56475f0342b9b
PixelMothman <63313404+pixelmothman@users.noreply.github.com>
2024-05-17 19:23:20 -0300

    feat: add isUserPartOfOrg, isUserPartOfOrgByUserEmail, getUsersInOrg, isUserOrgRole, isUserOnlyOwnerInOrg, and isUserRemovingThemselves fn to data.ts

--- utils/data.ts
+++ utils/data.ts
@@ -80,6 +80,322 @@ export async function getCurrentOrgName(orgId: string){
     } catch (e: any) {
         return handleError(e);
     };
+};
+
+
+// server-side function to check if user calling this function is part of the org
+export async function isUserPartOfOrg(orgID: string){
+    //prevents the response from being cached
+    noStore();
+    try {
+        let supabase = createClient()
+
+        //check the user exists
+        const { data, error } = await supabase.auth.getUser()
+        if (error || !data?.user) {
+            throw new AuthenticationError('User not found');
+        };
+        const user = data.user
+
+        //delete prvious instance of supabase client and start using supabase client with server role now
+        supabase = createSupaServerClient();
+
+        //check if the user belongs to the org
+        const {data: orgMembershipData, error: orgMembershipDataError} = await supabase.from("org_membership_table").select('org_id').eq('user_id', user.id).eq('org_id', orgID).neq('org_membership_status', 'pending');
+        if (orgMembershipDataError) {
+            throw new DBError('Failed to fetch org membership data');
+        };
+
+        //if the user is not a member of any orgs, return
+        if(!orgMembershipData || orgMembershipData.length === 0){
+            return {
+                isUserPartOfOrg: false
+            };
+        };
+
+        return {
+            isUserPartOfOrg: true
+        };
+    } catch (e: any) {
+        return handleError(e);
+    };
+};
+
+
+// server-side function to check if user is part of the org by user email
+export async function isUserPartOfOrgByUserEmail(orgID: string, userEmail:string){
+    //prevents the response from being cached
+    noStore();
+    try {
+        let supabase = createClient()
+
+        //check the user exists
+        const { data, error } = await supabase.auth.getUser()
+        if (error || !data?.user) {
+            throw new AuthenticationError('User not found');
+        };
+
+        //delete prvious instance of supabase client and start using supabase client with server role now
+        supabase = createSupaServerClient();
+
+        //check if the user belongs to the org by email
+        let {data: orgMembersData, error: orgMembersDataError} = await supabase.from("org_membership_table").select(`
+        org_id,
+        org_membership_role,
+        user_table!org_membership_table_user_id_fkey (
+            user_id,
+            user_email,
+            user_email_status
+        )
+        `).eq('org_id', orgID);
+        if (orgMembersDataError) {
+            throw new DBError('Failed to fetch org members data');
+        }
+
+        //if the org members data is not found, return
+        if(!orgMembersData || orgMembersData.length === 0){
+            return { isUserPartOfOrg: false };
+        };
+
+        //filter the org members data by user email
+        let userIsPartOfOrg: {orgID: string, userStatus: string, orgRole: string} | undefined;
+        for(let i=0; i < orgMembersData.length; i++){
+            if((orgMembersData[i].user_table as any).user_email === userEmail){
+                userIsPartOfOrg = {
+                    orgID: (orgMembersData[i].user_table as any).user_id,
+                    userStatus: (orgMembersData[i].user_table as any).user_email_status,
+                    orgRole: orgMembersData[i].org_membership_role
+                };
+                break;
+            }
+        };
+
+        //if the user is not a member of the org, return
+        if (!userIsPartOfOrg || Object.keys(userIsPartOfOrg).length === 0){
+            return { isUserPartOfOrg: false };
+        }
+
+        // Return true and user details if the user is part of the org
+        return {
+            isUserPartOfOrg: true,
+            userIDByEmail: userIsPartOfOrg.orgID,
+            userStatus: userIsPartOfOrg.userStatus,
+            userOrgRole: userIsPartOfOrg.orgRole
+        };
+    } catch (e: any) {
+        return handleError(e);
+    };
+};
+
+
+// external - server-side function to get the users in the org
+export async function getUsersInOrg(orgID: string){
+    //prevents the response from being cached
+    noStore();
+    try {
+        let supabase = createClient();
+
+        //check the user exists
+        const { data, error } = await supabase.auth.getUser()
+        if (error || !data?.user) {
+            throw new AuthenticationError('User not found');
+        };
+        const user = data.user
+
+        //delete previous instance of supabase client and start using supabase client with server role now
+        supabase = createSupaServerClient();
+
+        //check the user is part of the org 
+        const {data: orgMembershipData, error: orgMembershipDataError} = await supabase.from("org_membership_table").select().eq('org_id', orgID).eq('user_id', user.id).neq('org_membership_status', 'pending');
+        if (orgMembershipDataError || !orgMembershipData || orgMembershipData.length === 0) {
+            throw new DBError('Failed to check user membership data');
+        };
+        
+        //fetch the org members
+        const {data: orgMembersData, error: orgMembersDataError} = await supabase.from("org_membership_table").select(`
+        org_membership_role,
+        org_membership_status,
+        user_email: user_table!org_membership_table_user_id_fkey (user_email)
+        `).eq('org_id', orgID);
+        if (orgMembersDataError) {
+            console.log('orgMembersDataError: ', orgMembersDataError);
+            throw new DBError('Failed to fetch user members');
+        };
+        
+        if (!orgMembersData || orgMembersData.length === 0) {
+            return [];
+        }
+        
+        //return the users that are part of the org
+        let dataToReturn: {
+            userEmail: any;
+            userRole: any;
+            userStatus: any;
+        }[]  = orgMembersData.map((orgMember: any) => {
+            return {
+                userEmail: orgMember.user_email.user_email,
+                userRole: orgMember.org_membership_role,
+                userStatus: orgMember.org_membership_status
+            };
+        });
+        return dataToReturn
+    } catch (e: any) {
+        return handleError(e);
+    };
+};
+
+// server-side function to check if the user has the required role in the org
+export async function isUserOrgRole(orgID: string, userID: string, userOrgRole: string){
+    //prevents the response from being cached
+    noStore();
+    try {
+        let supabase = createClient()
+
+        //check the user exists
+        const { data, error } = await supabase.auth.getUser()
+        if (error || !data?.user) {
+            throw new AuthenticationError('User not found');
+        };
+
+        //delete prvious instance of supabase client and start using supabase client with server role now
+        supabase = createSupaServerClient();
+
+        //check if the user belongs to the org
+        const {data: orgMembershipData, error: orgMembershipDataError} = await supabase.from("org_membership_table").select('org_membership_role').eq('org_id', orgID).eq('user_id', userID).neq('org_membership_status', 'pending');
+        if (orgMembershipDataError) {
+            throw new DBError('Failed to fetch org membership data');
+        };
+
+        //if the user is not a member of the org, return
+        if(!orgMembershipData || orgMembershipData.length === 0){
+            return { isUserOrgRole: false };
+        };
+
+        //check if the user has the required role in the org
+        if(orgMembershipData[0].org_membership_role === userOrgRole){
+            return { isUserOrgRole: true };
+        };
+
+        return { isUserOrgRole: false };
+    } catch (e: any) {
+        return handleError(e);
+    };
+};
+
+
+// server-side function to check if the user being removed has the last owner role in the org
+export async function isUserOnlyOwnerInOrg(orgID: string, userEmail: string){
+    //prevents the response from being cached
+    noStore();
+    try {
+        let supabase = createClient()
+
+        //check the user exists
+        const { data, error } = await supabase.auth.getUser()
+        if (error || !data?.user) {
+            throw new AuthenticationError('User not found');
+        };
+
+        //delete prvious instance of supabase client and start using supabase client with server role now
+        supabase = createSupaServerClient();
+
+        //check if the user belongs to the org
+        let {data: orgMembersData, error: orgMembersDataError} = await supabase.from("org_membership_table").select(`
+        org_membership_role,
+        user_email: user_table!org_membership_table_user_id_fkey (user_email)
+        `).eq('org_id', orgID);
+        if (orgMembersDataError) {
+            throw new DBError('Failed to fetch org members data');
+        }
+
+        //if the org members data is not found, return
+        if(!orgMembersData || orgMembersData.length === 0){
+            return { isUserOnlyOwnerInOrg: false };
+        };
+  
+        //check if the user is part of the org and has the 'owner' role
+        let userIsOwner = false;
+        let ownerCount = 0;
+    
+        for (let i = 0; i < orgMembersData.length; i++) {
+            if ((orgMembersData[i].user_email as any).user_email === userEmail && orgMembersData[i].org_membership_role === 'owner') {
+            userIsOwner = true;
+            }
+            if (orgMembersData[i].org_membership_role === 'owner') {
+            ownerCount++;
+            }
+        }
+    
+        //check if the user is the only owner in the org
+        if (userIsOwner && ownerCount === 1) {
+            return { isUserOnlyOwnerInOrg: true };
+        }
+    
+        return { isUserOnlyOwnerInOrg: false };
+    } catch (e: any) {
+        return handleError(e);
+    };
 }
 
+
+// server side function to check the user is not trying to remove themselves if they are the only user in the trip 
+// or if the are the only confirmed user in the trip
+export async function isUserRemovingThemselves(orgID: string, userID: string){
+    //prevents the response from being cached
+    noStore();
+    try {
+        let supabase = createClient()
+
+        //check the user exists
+        const { data, error } = await supabase.auth.getUser()
+        if (error || !data?.user) {
+            throw new AuthenticationError('User not found');
+        };
+        const user = data.user
+
+        //delete prvious instance of supabase client and start using supabase client with server role now
+        supabase = createSupaServerClient();
+
+        //fetch the org members
+        const {data: orgMembersData, error: orgMembersDataError} = await supabase
+        .from("org_membership_table")
+        .select(`
+        org_membership_role, 
+        org_membership_status, 
+        user_table!org_membership_table_user_id_fkey (user_id)
+        `)
+        .eq('org_id', orgID)
+
+        //if the trip members data is not found, return
+        if(!orgMembersData || orgMembersData.length === 0 || orgMembersDataError){
+            throw new DBError('Failed to fetch org members');
+        };
+
+        // Process org members data
+        let orgMembers: {userID: string, orgMembershipStatus: string, orgRole: string}[] = orgMembersData.map(orgMember => ({
+            userID: (orgMember.user_table as any).user_id,
+            orgMembershipStatus: orgMember.org_membership_status,
+            orgRole: orgMember.org_membership_role
+        }));
+        //console.log('orgMembers: ', orgMembers);
+
+        // Check if the user is the only user or the only confirmed user in the org
+        if (orgMembers.length === 1 && orgMembers[0].userID === user.id) {
+            return true;
+        }
+
+        //if the user is trying to delete themselves and they are the only confirmed user in the org, return
+        if(orgMembers.length > 1){
+            let confirmedUsers = orgMembers.filter((orgMember) => orgMember.orgMembershipStatus === 'confirmed');
+            //console.log('confirmedUsers: ', confirmedUsers);
+            if(confirmedUsers.length === 1 && user.id === userID){
+                return true;
+            };
+        };
+
+        return false;
+    } catch (e: any) {
+        return handleError(e);
+    };
+};
 //---------------------------------------------------------------------
