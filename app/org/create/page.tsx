4597ca606e00bd47288d9a8e49439b99b47b4bc1
PixelMothman <63313404+pixelmothman@users.noreply.github.com>
2024-05-23 11:17:02 -0300

    refactor: Update CreateOrgPage to handle redirection if the user is already part of an org

--- app/org/create/page.tsx
+++ app/org/create/page.tsx
@@ -1,6 +1,17 @@
 import FormCreateFirstOrg from "@/components/org/formCreateFirstOrg";
+import { getOrgs } from "@/utils/data";
+import { redirect } from "next/navigation";
 
-export default function CreateOrgPage(){
+export default async function CreateOrgPage(){
+
+    const isUserPartOfOrg = await getOrgs();
+    if ('error' in isUserPartOfOrg!) {
+        redirect(`/org/error?error_message=${isUserPartOfOrg.error.message}&error_code=${isUserPartOfOrg.error.code}&error_details=${isUserPartOfOrg.error.details}`)      
+    };
+    if (isUserPartOfOrg.length > 0) {
+        redirect(`/dashboard`);
+    };
+
     return (
         <div className="w-full h-full flex justify-center mt-64">
             <div className="w-96 h-fit">
