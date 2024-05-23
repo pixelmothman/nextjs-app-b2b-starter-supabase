f87756f3e9dbcd5d8fd6447515142748cd63eff4
PixelMothman <63313404+pixelmothman@users.noreply.github.com>
2024-05-23 14:38:39 -0300

    refactor: update OrgPage page to include basic structure in app/dashboard/[orgID]

--- app/dashboard/[orgID]/page.tsx
+++ app/dashboard/[orgID]/page.tsx
@@ -1,6 +1,15 @@
 export default function OrgPage(){
     return (
-        <>
-        </>
+        <div className="w-full h-full p-4">
+            <div className="w-full h-full p-6 bg-neutral-50 shadow-[0px_0px_4px_2px_rgba(0,0,0,0,3)] shadow-neutral-300 rounded-sm overflow-hidden">
+                <div className="flex flex-col gap-4 overflow-y-auto">
+                    <div className="flex flex-col gap-2">
+                        <h1 className="text-2xl font-bold text-neutral-900">Home</h1>
+                        <div className="flex flex-col">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
