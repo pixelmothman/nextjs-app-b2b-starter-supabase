d7e95780232230242b917e95bf73ff1098cbf6ff
PixelMothman <63313404+pixelmothman@users.noreply.github.com>
2024-05-25 15:54:45 -0300

    fix: fix the TypeScript Errors in the FormButtonAbstraction component

--- components/miscelaneous/formButtonAbstraction.tsx
+++ components/miscelaneous/formButtonAbstraction.tsx
@@ -7,12 +7,12 @@ interface FormButtonAbstractionProps {
     buttonText?: string;
     buttonIcon?: string;
     background?: boolean;
-}
+};
 
 export default function FormButtonAbstraction({loadingText, buttonText, buttonIcon, background = true }: FormButtonAbstractionProps){
     const { pending } = useFormStatus();
 
-    const buttonIcons = {
+    const buttonIcons: {[key: string]: JSX.Element} = {
         'delete': <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 256 256"><path d="M216,48H40a12,12,0,0,0,0,24h4V208a20,20,0,0,0,20,20H192a20,20,0,0,0,20-20V72h4a12,12,0,0,0,0-24ZM188,204H68V72H188ZM76,20A12,12,0,0,1,88,8h80a12,12,0,0,1,0,24H88A12,12,0,0,1,76,20Z"></path></svg>,
         'rightArrow': <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 256 256"><path d="M224.49,136.49l-72,72a12,12,0,0,1-17-17L187,140H40a12,12,0,0,1,0-24H187L135.51,64.48a12,12,0,0,1,17-17l72,72A12,12,0,0,1,224.49,136.49Z"></path></svg>
     }
